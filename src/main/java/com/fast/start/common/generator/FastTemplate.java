e3b9d84f3cca7c2ca1bd4ee2b6cc1c44f9a5622e
zoe <2259965328@qq.com>
2023-08-14 22:06:48 +0800

    代码生成开发

---
+++ src/main/java/com/fast/start/common/generator/FastTemplate.java
@@ -0,0 +1,11 @@
+package com.fast.start.common.generator;
+
+/**
+ * @author zoe
+ * @date 2023/8/13
+ * @description
+ */
+public interface FastTemplate {
+
+    void generator();
+}
---
+++ src/main/java/com/fast/start/common/generator/Template1.java
@@ -0,0 +1,16 @@
+package com.fast.start.common.generator;
+
+/**
+ * @author zoe
+ * @date 2023/8/13
+ * @description
+ */
+public class Template1 implements FastTemplate {
+
+    public Template1() {}
+
+    @Override
+    public void generator() {
+        System.out.println("1111 = " + 1111);
+    }
+}
